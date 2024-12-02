344157c70d5cd828ba8e13562080be360dc16445
anamsmartins <anaritamsmartins@gmail.com>
2024-12-02 16:38:06 +0000

    Puzzle 1 improvements

--- Day2/Puzzle1/puzzle.js
+++ Day2/Puzzle1/puzzle.js
@@ -3,56 +3,37 @@ import { readInput } from "../../utils.js";
 var input = readInput();
 
 var reports = input.split("\n");
-var safeReports = 0;
-var numberLevelsPerReport;
 
-reports.forEach((report) => {
-  var levels = report.split(" ");
-  numberLevelsPerReport = levels.length;
-
-  var safe = true;
-  var increasing = true;
-
-  levels.forEach((level, index) => {
-    if (index == numberLevelsPerReport - 1) {
-      return;
+function isSafe(difference, increasing) {
+  if (increasing) {
+    if (difference < 0 || difference < 1 || difference > 3) {
+      return false;
     }
-
-    var levelInt = parseInt(level);
-    var nextLevelInt = parseInt(levels[index + 1]);
-
-    if (index == 0 && levelInt > nextLevelInt) {
-      increasing = false;
+  } else {
+    if (
+      difference > 0 ||
+      Math.abs(difference) < 1 ||
+      Math.abs(difference) > 3
+    ) {
+      return false;
     }
-
-    var difference = nextLevelInt - levelInt;
-
-    if (increasing) {
-      if (difference < 0) {
-        safe = false;
-        return;
-      }
-
-      if (difference < 1 || difference > 3) {
-        safe = false;
-        return;
-      }
-    } else {
-      if (difference > 0) {
-        safe = false;
-        return;
-      }
-
-      if (Math.abs(difference) < 1 || Math.abs(difference) > 3) {
-        safe = false;
-        return;
-      }
-    }
-  });
-
-  if (safe) {
-    safeReports++;
   }
+
+  return true;
+}
+
+function isReportSafe(levels) {
+  var increasing = parseInt(levels[1]) > parseInt(levels[0]);
+  var safeLevels = levels.map((level, index) => {
+    var difference = parseInt(levels[index + 1]) - parseInt(level);
+    return isSafe(difference, increasing);
+  });
+  return !safeLevels.includes(false);
+}
+
+var safeReportsPart1 = reports.filter((report) => {
+  var levels = report.split(" ");
+  return isReportSafe(levels);
 });
 
-console.log(safeReports);
+console.log("Part 1: " + safeReportsPart1.length);
